1c065c6d7dd5e49884c54fc82758c58f4e144dea
anlin <anlin@anlinshia>
2024-07-30 19:55:17 +0530

    commit

---
+++ src/main/java/name/lastproject/ExcelCode.java
@@ -0,0 +1,34 @@
+package name.lastproject;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+public class ExcelCode {
+
+public static FileInputStream f;
+public static XSSFWorkbook wb; 
+public static XSSFSheet sh;
+
+public static String readStringData(int i, int j) throws IOException {
+f= new FileInputStream("C:\\Users\\anlin\\Downloads\\Excel.xlsx");
+wb = new XSSFWorkbook(f);
+sh = wb. getSheet ("Sheet1");
+XSSFRow r = sh. getRow(i);
+XSSFCell c = r.getCell(j);
+return c.getStringCellValue();
+}
+
+
+public static double readNumericData(int i, int j) throws IOException {
+f= new FileInputStream("C:\\Users\\anlin\\Downloads\\Excel.xlsx");
+wb = new XSSFWorkbook(f);
+sh = wb.getSheet ("Sheet1");
+XSSFRow r= sh.getRow(i);
+XSSFCell c= r.getCell(j);
+return c.getNumericCellValue();
+}
+}
---
+++ src/main/java/name/lastproject/TestExcel.java
@@ -0,0 +1,11 @@
+package name.lastproject;
+
+	import java.io.IOException;
+	public class TestExcel {
+	public static void main (String[] args) throws IOException {
+	String s = ExcelCode.readStringData(1, 0);
+	System.out.println(s);
+	double d = ExcelCode.readNumericData(1,1);
+	System.out.println(d);
+}
+	}
